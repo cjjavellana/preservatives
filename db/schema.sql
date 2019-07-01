09e848a79bd73fc24520c38bb72df8b0d6752d26
Christian Javellana <cjjavellana@gmail.com>
2019-07-02 07:04:30 +0800

    Add user table columns

--- db/schema.sql
+++ db/schema.sql
@@ -2,5 +2,13 @@ use ServiceMonitor;
 drop table if exists users;
 create table users (
     id int not null auto_increment,
+    username varchar(16) not null,
+    password varchar(512) not null,
+    first_name varchar(128) not null,
+    last_name varchar(128) null,
+    created_by varchar(16) not null default 'SYSTEM',
+    created_date timestamp not null default now(),
+    updated_by varchar(16) null,
+    updated_date timestamp null,
     PRIMARY KEY(id)
 );
